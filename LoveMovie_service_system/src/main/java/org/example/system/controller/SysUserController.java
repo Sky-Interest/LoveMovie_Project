3eb6fffaf6333a270600936b6258f9178e53a47f
Sky_Interest <841102500@qq.com>
2024-09-20 19:57:00 +0800

    13.2.1用户管理模块开发 后端开发 编写控制类

--- LoveMovie_service_system/src/main/java/org/example/system/controller/SysUserController.java
+++ LoveMovie_service_system/src/main/java/org/example/system/controller/SysUserController.java
@@ -1,7 +1,17 @@
 package org.example.system.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.example.model.vo.SysUserQueryVo;
+import org.example.system.entity.SysUser;
+import org.example.system.service.SysUserService;
+import org.example.system.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +28,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "用户管理控制器")
 @RequestMapping("/admin/system/sysUser")
 public class SysUserController {
+    @Autowired
+    private SysUserService sysUserService;
 
+    // 分页和条件查询
+    @ApiOperation("分页和条件查询")
+    @GetMapping("/{page}/{limit}")
+    public Result selectUserPageByVo(@PathVariable Long page,
+                                     @PathVariable Long limit,
+                                     SysUserQueryVo sysUserQueryVo) {
+        IPage<SysUser> iPage = new Page<>(page, limit);
+
+        iPage = this.sysUserService.selectPage(iPage, sysUserQueryVo);
+
+        return Result.ok(iPage);
+    }
 }
 
