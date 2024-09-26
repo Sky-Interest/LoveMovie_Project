c55fd6eb0a80f82f1dd0460d23f6e64df703a79b
Sky_Interest <841102500@qq.com>
2024-09-26 10:37:56 +0800

    17.2.3影视管理功能开发 影视内容管理开发 通过dotenv依赖，使用环境变量引入AccessKey保证安全

--- LoveMovie_service_system/src/main/java/org/example/ServiceApplication.java
+++ LoveMovie_service_system/src/main/java/org/example/ServiceApplication.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("org.example.system.mapper")
 public class ServiceApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("ALIYUN_ACCESS_KEY_ID", dotenv.get("ALIYUN_ACCESS_KEY_ID"));
+        System.setProperty("ALIYUN_ACCESS_KEY_SECRET", dotenv.get("ALIYUN_ACCESS_KEY_SECRET"));
         SpringApplication.run(ServiceApplication.class,args);
     }
 }
